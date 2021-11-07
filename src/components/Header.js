3edeb2162096da52049722165bb05ffd7310a54f
Lia OBrien <liaobrien123@gmail.com>
2021-11-07 15:44:39 -0500

    created very basic header which now renders to page

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+      return (
+            <header class="header navbar">
+                  <h1>Lia O'Brien</h1>
+            </header>
+      )
+}
