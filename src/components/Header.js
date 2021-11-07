2d27c5986c7c8b0222c83fbee70a49a4510466d3
Lia OBrien <liaobrien123@gmail.com>
2021-11-07 16:06:03 -0500

    created nav component and imported it inside header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from './Navigation';
 
 export default function Header() {
       return (
             <header class="header navbar">
                   <h1>Lia O'Brien</h1>
+                  <Navigation />
             </header>
       )
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Navigation() {
+      return (
+            <nav>
+                  <ul>
+                        <li><a href="#about-me">About Me</a></li>
+                        <li><a href="#work">Work</a></li>
+                        <li><a href="#contact">Contact Me</a></li>
+                        <li>
+                              <a
+                                    href="https://docs.google.com/document/d/1nG6It3SZ4QYIcA0Aa--fMz31v48jBWUyXAUwGeDZYC0/edit?usp=sharing"
+                                    target="_blank"
+                                    rel="noopener"
+                              >Resume</a
+                              >
+                        </li>
+                  </ul>
+            </nav>
+      )
+}
