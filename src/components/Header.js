95afe04a7f5ecf6c7cc1fa35d61666dd2df9ba29
Lia OBrien <liaobrien123@gmail.com>
2021-11-07 16:16:39 -0500

    changed class to className in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 
 export default function Header() {
       return (
-            <header class="header navbar">
+            <header className="header navbar">
                   <h1>Lia O'Brien</h1>
                   <Navigation />
             </header>
