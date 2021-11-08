f8e6b14cca886464c5a78419a4d5c84600a47c22
Lia OBrien <liaobrien123@gmail.com>
2021-11-07 19:39:13 -0500

    created footer icons

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+      return (
+            <footer>
+                  <ul className="footer d-flex justify-content-center">
+                        <li><i class="bi bi-github mx-3" role="img" aria-label="GitHub"></i></li>
+                        <li><i class="bi bi-linkedin mx-3" role="img" aria-label="LinkedIn"></i></li>
+                        <li><i class="bi bi-stack-overflow mx-3" role="img" aria-label="Stack Overflow"></i></li>
+                  </ul>
+            </footer>
+      )
+}
