a7be6aec5b9dd589e107b6b04a66e592bf9e3a8b
Lia OBrien <liaobrien123@gmail.com>
2021-11-08 15:52:36 -0500

    created projects array of objects to iterate over for cards

---
+++ src/components/Project.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const projects = [
+      {
+            title: 'Decked Out',
+            github: 'Decked-Out',
+            deploy: 'https://hidden-island-92263.herokuapp.com/'
+      },
+      {
+            title: 'Pack Rat',
+            github: 'Pack-Rat',
+            deploy: 'https://liaobrien.github.io/Pack-Rat/'
+      },
+      {
+            title: 'Tech Blog',
+            github: 'tech-blog',
+            deploy: 'https://fast-chamber-00133.herokuapp.com/'
+      },
+      {
+            title: 'Weather Dashboard',
+            github: 'weather-dashboard',
+            deploy: 'https://liaobrien.github.io/weather-dashboard'
+      },
+      {
+            title: 'Budget Tracker',
+            github: 'offline-budget-tracker',
+            deploy: 'https://hidden-badlands-92774.herokuapp.com/'
+      },
+      {
+            title: 'Express Note Taker',
+            github: 'note-taker',
+            deploy: 'https://pacific-hamlet-03759.herokuapp.com/'
+      }
+]
+
+export default function Project() {
+
+}
