e3c30fa1d794a355eb15157dfde2267c271e6803
Lia OBrien <liaobrien123@gmail.com>
2021-11-08 09:20:19 -0500

    moved jsx in navigation inside header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import Navigation from './Navigation';
 
 export default function Header() {
       return (
             <header className="header navbar">
                   <h1>Lia O'Brien</h1>
-                  <Navigation />
+                  <nav>
+                        <ul>
+                              <li><a href="#about-me">About Me</a></li>
+                              <li><a href="#work">Work</a></li>
+                              <li><a href="#contact">Contact Me</a></li>
+                              <li>
+                                    <a
+                                          href="https://docs.google.com/document/d/1nG6It3SZ4QYIcA0Aa--fMz31v48jBWUyXAUwGeDZYC0/edit?usp=sharing"
+                                          target="_blank"
+                                          rel="noreferrer"
+                                    >Resume</a
+                                    >
+                              </li>
+                        </ul>
+                  </nav>
             </header>
       )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// note: move all the jsx in here to header.js; this will hold my different pages i think (about, contact, etc.)
 export default function Navigation() {
       return (
             <nav>
