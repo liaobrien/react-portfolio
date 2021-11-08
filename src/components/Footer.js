05563ec5d86ebf9106571cc25faa9e6da8536eea
Lia OBrien <liaobrien123@gmail.com>
2021-11-07 19:55:24 -0500

    fixed class vs className errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ export default function Footer() {
             <footer>
                   <ul className="footer d-flex justify-content-center">
                         <li>
-                              <a href="https://github.com/liaobrien"><i class="bi bi-github mx-3" role="img" aria-label="GitHub"></i></a>
+                              <a href="https://github.com/liaobrien"><i className="bi bi-github mx-3" role="img" aria-label="GitHub"></i></a>
                         </li>
                         <li>
-                              <a href="https://www.linkedin.com/in/lia-obrien"><i class="bi bi-linkedin mx-3" role="img" aria-label="LinkedIn"></i></a>
+                              <a href="https://www.linkedin.com/in/lia-obrien"><i className="bi bi-linkedin mx-3" role="img" aria-label="LinkedIn"></i></a>
                         </li>
                         <li>
-                              <a href="https://stackoverflow.com/users/16589157/lobrien"><i class="bi bi-stack-overflow mx-3" role="img" aria-label="Stack Overflow"></i></a>
+                              <a href="https://stackoverflow.com/users/16589157/lobrien"><i className="bi bi-stack-overflow mx-3" role="img" aria-label="Stack Overflow"></i></a>
                         </li>
                   </ul>
             </footer>
