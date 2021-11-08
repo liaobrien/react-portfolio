7e41124f942d388e6b7292ef53312eaca9852a60
Lia OBrien <liaobrien123@gmail.com>
2021-11-07 19:54:15 -0500

    changed footer icon size, turned them into working links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,15 @@ export default function Footer() {
       return (
             <footer>
                   <ul className="footer d-flex justify-content-center">
-                        <li><i class="bi bi-github mx-3" role="img" aria-label="GitHub"></i></li>
-                        <li><i class="bi bi-linkedin mx-3" role="img" aria-label="LinkedIn"></i></li>
-                        <li><i class="bi bi-stack-overflow mx-3" role="img" aria-label="Stack Overflow"></i></li>
+                        <li>
+                              <a href="https://github.com/liaobrien"><i class="bi bi-github mx-3" role="img" aria-label="GitHub"></i></a>
+                        </li>
+                        <li>
+                              <a href="https://www.linkedin.com/in/lia-obrien"><i class="bi bi-linkedin mx-3" role="img" aria-label="LinkedIn"></i></a>
+                        </li>
+                        <li>
+                              <a href="https://stackoverflow.com/users/16589157/lobrien"><i class="bi bi-stack-overflow mx-3" role="img" aria-label="Stack Overflow"></i></a>
+                        </li>
                   </ul>
             </footer>
       )
