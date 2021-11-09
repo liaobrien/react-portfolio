e965f008ad800ca50c446f921de2108f80860bf2
Lia OBrien <liaobrien123@gmail.com>
2021-11-08 19:39:39 -0500

    created about me page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 // import Project from './components/Project';
 // import Contact from './components/Contact';
+// import About from './components/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       {/* <Project /> */}
       {/* <Contact /> */}
+      {/* <About /> */}
       <Footer />
     </>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,20 @@ import React from "react";
 
 export default function About() {
       return (
-            <div id="about-me">
-
+            <div id="about-me" className="d-flex container justify-content-center align-items-center my-5">
+                  <img src="./assets/images/headshot-min.jpg" id="headshot" className="mx-3" alt="headshot" />
+                  <div>
+                        <h2>About Me</h2>
+                        <p>I'm a full stack web developer from Newark, Delaware. Shortly before
+                              graduating from the University of Delaware with my bachelor's degree in
+                              Media Communication, I realized I wanted to be more technically
+                              challenged in my work. After taking a computer science class on a whim
+                              my last semester, I fell in love with coding and knew it was something I
+                              wanted to pursue further. Currently I'm working toward a certificate in
+                              Full Stack Web Development from the University of Pennsylvania, and I
+                              plan to look for a full-time position in web development after
+                              graduation.</p>
+                  </div>
             </div>
       )
 }
