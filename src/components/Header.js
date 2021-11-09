bee0efd81ce41e61e14444f37422208ba30e29d4
Lia OBrien <liaobrien123@gmail.com>
2021-11-08 20:53:30 -0500

    reworked navigation component to include conditional rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,6 @@ export default function Header() {
       return (
             <header className="header navbar">
                   <h1>Lia O'Brien</h1>
-                  <nav>
-                        <ul>
-                              <li><a href="#about-me">About Me</a></li>
-                              <li><a href="#work">Work</a></li>
-                              <li><a href="#contact">Contact Me</a></li>
-                              <li>
-                                    <a
-                                          href="https://docs.google.com/document/d/1nG6It3SZ4QYIcA0Aa--fMz31v48jBWUyXAUwGeDZYC0/edit?usp=sharing"
-                                          target="_blank"
-                                          rel="noreferrer"
-                                    >Resume</a
-                                    >
-                              </li>
-                        </ul>
-                  </nav>
             </header>
       )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,32 @@
 import React from 'react';
 // note: move all the jsx in here to header.js; this will hold my different pages i think (about, contact, etc.)
-export default function Navigation() {
+export default function Navigation({ currentPage, handlePageChange }) {
+      return (
+            <nav>
+                  <ul>
+                        <li><a href="#about-me"
+                              onClick={() => handlePageChange('About Me')}
 
+                              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                        >About Me</a></li>
+                        <li><a href="#work"
+                              onClick={() => handlePageChange('Work')}
+
+                              className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
+                        >Work</a></li>
+                        <li><a href="#contact"
+                              onClick={() => handlePageChange('Contact')}
+
+                              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact Me</a></li>
+                        <li>
+                              <a
+                                    href="https://docs.google.com/document/d/1nG6It3SZ4QYIcA0Aa--fMz31v48jBWUyXAUwGeDZYC0/edit?usp=sharing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                              >Resume</a
+                              >
+                        </li>
+                  </ul>
+            </nav>
+      )
 }
