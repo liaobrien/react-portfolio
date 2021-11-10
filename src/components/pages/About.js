dbe06f90bd845373825b567300167be41d0ab5dd
Lia OBrien <liaobrien123@gmail.com>
2021-11-10 14:54:40 -0500

    edited about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,19 +3,11 @@ import headshot from '../../assets/images/headshot-min.jpg'
 
 export default function About() {
       return (
-            <div id="about-me" className="d-flex container justify-content-center align-items-center my-5">
-                  <img src={headshot} id="headshot" className="mx-3" alt="headshot" />
+            <div id="about-me" className="container justify-content-center my-5">
+                  <img src={headshot} id="headshot" className="mx-auto mb-5 d-block" alt="headshot" />
                   <div>
-                        <h2>About Me</h2>
-                        <p>I'm a full stack web developer from Newark, Delaware. Shortly before
-                              graduating from the University of Delaware with my bachelor's degree in
-                              Media Communication, I realized I wanted to be more technically
-                              challenged in my work. After taking a computer science class on a whim
-                              my last semester, I fell in love with coding and knew it was something I
-                              wanted to pursue further. Currently I'm working toward a certificate in
-                              Full Stack Web Development from the University of Pennsylvania, and I
-                              plan to look for a full-time position in web development after
-                              graduation.</p>
+                        <h2 className="text-center">Hi, I'm Lia.</h2>
+                        <p className="text-center">I'm a full stack web developer from Newark, Delaware with a professional background in journalism. I have a Bachelor of Arts in Media Communication from the University of Delaware, but my interest in coding began when I took a computer science class my last semester of college. From that point, I fell in love with programming as it combined my two favorite things: creativity and problem solving! Currently I am working towards a certificate in Full Stack Web Development at the University of Pennsylvania, and I can't wait to start working in the tech field after my graduation in November 2021.</p>
                   </div>
             </div>
       )
