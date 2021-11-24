7b5f8c48eb44d5d3fff2d7fc2e3bd86a873db84d
Lia OBrien <liaobrien123@gmail.com>
2021-11-24 15:12:00 -0500

    changed stack overflow footer link to email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ export default function Footer() {
             <footer>
                   <ul className="footer d-flex justify-content-center">
                         <li>
+                              <a href="mailto:liaobrien123@gmail.com"><i className="bi bi-envelope-fill mx-3" role="img" aria-label="Email"></i></a>
+                        </li>
+                        <li>
                               <a href="https://github.com/liaobrien"><i className="bi bi-github mx-3" role="img" aria-label="GitHub"></i></a>
                         </li>
                         <li>
                               <a href="https://www.linkedin.com/in/lia-obrien"><i className="bi bi-linkedin mx-3" role="img" aria-label="LinkedIn"></i></a>
-                        </li>
-                        <li>
-                              <a href="https://stackoverflow.com/users/16589157/lobrien"><i className="bi bi-stack-overflow mx-3" role="img" aria-label="Stack Overflow"></i></a>
                         </li>
                   </ul>
             </footer>
