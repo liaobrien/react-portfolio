487d70625c32549eb81f07b4808c790d767c2a82
Lia OBrien <liaobrien123@gmail.com>
2021-11-24 14:48:05 -0500

    gave description to each project, made text centered

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -11,8 +11,8 @@ const projects = [
             id: 1,
             title: 'BingeWorthy',
             image: Bingeworthy,
-            description: 'MERN Stack',
-            tools: '',
+            description: 'MERN Stack application where users can search any movie by title to create their own personal watchlist.',
+            tools: 'React | Bootstrap | GraphQL | MongoDB | Mongoose | Server-side API',
             deploy: 'https://nameless-castle-76240.herokuapp.com/',
             github: 'https://github.com/liaobrien/bingeworthy'
       },
@@ -20,8 +20,8 @@ const projects = [
             id: 2,
             title: 'Decked Out',
             image: DeckedOut,
-            description: 'test',
-            tools: '',
+            description: "Full stack app similar to Quizlet where users can create, update, and delete study decks and interact with other users' decks.",
+            tools: 'Handlebars | Bootstrap | Node | Express | MySQL | Sequelize | REST API',
             deploy: 'https://hidden-island-92263.herokuapp.com/',
             github: 'https://github.com/liaobrien/Decked-Out'
       },
@@ -29,8 +29,8 @@ const projects = [
             id: 3,
             title: 'Pack Rat',
             image: PackRat,
-            description: 'test',
-            tools: '',
+            description: 'Front-end application that displays NASA satellite imagery and current weather for any National Park in the U.S. Also features a packing list feature that the user creates dynamically.',
+            tools: 'HTML | Bulma | jQuery | Server-side API',
             deploy: 'https://liaobrien.github.io/Pack-Rat/',
             github: 'https://github.com/liaobrien/Pack-Rat'
       },
@@ -38,8 +38,8 @@ const projects = [
             id: 4,
             title: 'Tech Blog',
             image: TechBlog,
-            description: 'test',
-            tools: '',
+            description: 'Full stack CMS blog that allows users to create, edit, and delete blog posts via authentication, as well as comment on posts.',
+            tools: 'Handlebars | Bootstrap | Node | Express | MySQL | Sequelize | REST API',
             deploy: 'https://fast-chamber-00133.herokuapp.com/',
             github: 'https://github.com/liaobrien/tech-blog'
       },
@@ -47,8 +47,8 @@ const projects = [
             id: 5,
             title: 'Weather Dashboard',
             image: WeatherDash,
-            description: 'test',
-            tools: '',
+            description: 'Front-end application that calls the current weather and 5-day forecast of any city entered or previously searched by the user.',
+            tools: 'HTML | Bootstrap | jQuery | Server-side API',
             deploy: 'https://liaobrien.github.io/weather-dashboard',
             github: 'https://github.com/liaobrien/weather-dashboard'
       },
@@ -56,8 +56,8 @@ const projects = [
             id: 6,
             title: 'Budget Tracker',
             image: BudgetTracker,
-            description: 'test',
-            tools: '',
+            description: "An application for tracking a user's budget refactored into a PWA.",
+            tools: 'HTML | Bootstrap | MongoDB | Mongoose | Node | Express',
             deploy: 'https://hidden-badlands-92774.herokuapp.com/',
             github: 'https://github.com/liaobrien/offline-budget-tracker'
       },
@@ -74,15 +74,16 @@ export default function Project() {
                   <div className="row justify-content-center my-4">
                         {
                               projects.map((project) => (
-                                    <div key={project.id} className="card p-0 col-lg-3 m-3">
+                                    <div key={project.id} className="card p-0 col-lg-4 m-3 text-center">
                                           <img
                                                 src={project.image}
                                                 className="card-img-top"
                                                 alt="screenshot"
                                           />
                                           <div className="card-body">
-                                                <h5 className="card-title">{project.title}</h5>
+                                                <h5 className="card-title"><strong>{project.title}</strong></h5>
                                                 <p>{project.description}</p>
+                                                <p><strong>Tools:</strong> <br /> {project.tools}</p>
                                                 <a
                                                       href={project.deploy}
                                                       className="btn link m-1"
