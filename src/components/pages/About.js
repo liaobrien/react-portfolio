20c8480dc3fa545c85484002bde9a32382372b5c
Lia OBrien <liaobrien123@gmail.com>
2021-11-26 10:56:21 -0500

    updated about me bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,8 +6,8 @@ export default function About() {
             <div id="about-me" className="container justify-content-center my-5">
                   <img src={headshot} id="headshot" className="mx-auto mb-5 d-block" alt="headshot" />
                   <div>
-                        <h2 className="text-center">Hi, I'm Lia.</h2>
-                        <p className="text-center">I'm a full stack web developer from Newark, Delaware with a professional background in journalism. I have a Bachelor of Arts in Media Communication from the University of Delaware, but my interest in coding began when I took a computer science class my last semester of college. From that point, I fell in love with programming as it combined my two favorite things: creativity and problem solving! Currently I am working towards a certificate in Full Stack Web Development at the University of Pennsylvania, and I can't wait to start working in the tech field after my graduation in November 2021.</p>
+                        <h2 className="text-center">About Me</h2>
+                        <p className="text-center">My name is Lia, and I'm a full stack web developer from Newark, Delaware with a special interest in frontend development. I earned by bachelor's degree in Media Communication from the University of Delaware in 2021, and during my studies I spent most of my time working as a news reporter and producer for my school's TV station. My interest in coding began when I took a computer science class my last semester of college, as I was looking to be challenged more technically. From that point, I fell in love with programming as it combined my two favorite things: creativity and problem solving! I now have a certificate in Full Stack Web Development from the University of Pennsylvania Coding Bootcamp, and I'm excited to start my career in this ever-changing field.</p>
                   </div>
             </div>
       )
